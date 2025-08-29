28cf0b89b439f7c1dcb44bf169b80ff0a3cdb3ed
aditya yadav <dedomil@tuta.io>
2025-08-29 18:16:25 +0530

    add google auth checker

--- server/src/routes/application.ts
+++ server/src/routes/application.ts
@@ -5,8 +5,10 @@ import db from "../database/index.js";
 import { metadata, orders, files } from "../database/schema.js";
 import { eq } from "drizzle-orm";
 import { razorpay } from "../razorpay.js";
+import { OAuth2Client } from "google-auth-library";
 
 const app = new Hono();
+const client = new OAuth2Client();
 
 app.post(
   "/upload",
@@ -26,35 +28,49 @@ app.post(
     ),
   ),
   async (c) => {
-    const configs = c.req.valid("json");
-    const data = []; // files data
+    try {
+      const authHeader = c.req.header("xxx-auth-token");
+      if (!authHeader) return c.json({ message: "failed" }, 500);
 
-    for (const config of configs) {
-      const metadataResponse = await db.query.metadata.findFirst({
-        where: eq(metadata.file, config.file),
-        columns: { pages: true },
+      const ticket = await client.verifyIdToken({
+        idToken: authHeader,
+        audience: process.env.GOOGLE_AUTH_ID,
       });
 
-      if (!metadataResponse) return c.json({ message: "file not found" });
+      const payload = ticket.getPayload();
 
-      data.push({ pages: metadataResponse.pages, ...config });
+      const configs = c.req.valid("json");
+      const data = []; // files data
+
+      for (const config of configs) { // for now O(1)
+        const metadataResponse = await db.query.metadata.findFirst({
+          where: eq(metadata.file, config.file),
+          columns: { pages: true },
+        });
+
+        if (!metadataResponse) return c.json({ message: "file not found" });
+
+        data.push({ pages: metadataResponse.pages, ...config });
+      }
+
+      // todo:
+      // user id matlab email
+      // insert into orders with user id and get order id
+      // await db.insert(orders).values({});
+
+      // insert with order id and user id in files
+      // await db.insert(files).values({ });
+
+      const response = await razorpay.orders.create({
+        amount: "100",
+        currency: "INR",
+        receipt: "payment for print #1",
+      });
+
+      return c.json({ data, response });
+    } catch (e) {
+      return c.json({ message: "invalid login" }, 500);
     }
-
-    // todo:
-    // user id matlab email
-    // insert into orders with user id and get order id
-    // await db.insert(orders).values({});
-
-    // insert with order id and user id in files
-    // await db.insert(files).values({ });
-
-    const response = await razorpay.orders.create({
-      amount: "100",
-      currency: "INR",
-      receipt: "payment for print #1",
-    });
-
-    return c.json({ data, response });
   },
 );
 
--- server/src/routes/upload.ts
+++ server/src/routes/upload.ts
@@ -4,43 +4,55 @@ import { s3Client } from "../s3.js";
 import db from "../database/index.js";
 import { metadata } from "../database/schema.js";
 import { extractMetadataAndPages } from "pdf-metadata";
+import { OAuth2Client } from "google-auth-library";
 
 const app = new Hono();
+const client = new OAuth2Client();
 
-// todo: add auth (any user can upload a file and get a filename)
 app.post("/upload", async (c) => {
-  const { file } = await c.req.parseBody();
-  
-  console.log(file)
+  try {
+    const authHeader = c.req.header("xxx-auth-token");
+    if (!authHeader) return c.json({ message: "auth header not present" }, 500);
 
-  if (!(file instanceof File)) return c.text("invalid file type", 200);
-  if (file.type != "application/pdf") return c.text("only pdf support", 200);
-  if (file.size > 10 * 1024 * 1024) return c.text("10mb limit", 200);
+    const ticket = await client.verifyIdToken({
+      idToken: authHeader,
+      audience: process.env.GOOGLE_AUTH_ID,
+    });
 
-  const arrayBuffer = await file.arrayBuffer();
-  const uint8Array = new Uint8Array(arrayBuffer);
-  const filename = `${crypto.randomUUID()}`;
+    const payload = ticket.getPayload();
 
-  const command = new PutObjectCommand({
-    Bucket: process.env.BUCKET,
-    Key: filename,
-    Body: uint8Array,
-    ContentType: file.type,
-    ContentLength: uint8Array.length,
-  });
+    const { file } = await c.req.parseBody();
 
-  const result = await s3Client.send(command);
+    if (!(file instanceof File)) return c.text("invalid file type", 200);
+    if (file.type != "application/pdf") return c.text("only pdf support", 200);
+    if (file.size > 10 * 1024 * 1024) return c.text("10mb limit", 200);
 
-  if (result.$metadata.httpStatusCode == 200) {
+    const arrayBuffer = await file.arrayBuffer();
+    const uint8Array = new Uint8Array(arrayBuffer);
+    const filename = `${crypto.randomUUID()}`;
+
+    const command = new PutObjectCommand({
+      Bucket: process.env.BUCKET,
+      Key: filename,
+      Body: uint8Array,
+      ContentType: file.type,
+      ContentLength: uint8Array.length,
+    });
+
+    const result = await s3Client.send(command);
+
+    if (result.$metadata.httpStatusCode != 200)
+      return c.json({ message: "failed" }, 500);
+
     await db.insert(metadata).values({
       file: filename,
       pages: (await extractMetadataAndPages(arrayBuffer)).pages,
     });
 
     return c.json({ file: filename });
+  } catch (e) {
+    return c.json({ message: "invalid login" }, 500);
   }
-
-  return c.json({ message: "failed" }, 500);
 });
 
 export default app;
